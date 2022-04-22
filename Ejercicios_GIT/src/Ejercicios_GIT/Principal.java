e1e86f4780ef933f1e58d25a0c3d20b533ab118f
Andrea <andreamariaromero.daw@ciudadescolarfp.es>
2022-04-22 13:44:14 +0200

    Error porque tengo la clase en la otra rama

--- Ejercicios_GIT/src/Ejercicios_GIT/Principal.java
+++ Ejercicios_GIT/src/Ejercicios_GIT/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Listado de alumnos 1º DAW");
 		System.out.println("Alumno: ");
+		Alumno al1 = new Alumno("Pepe", "Sanz", "458564515M");
 	}
 
 }
