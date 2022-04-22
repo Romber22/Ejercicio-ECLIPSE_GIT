96280d14413a19c1bd917b621a4b7f3c2411687f
Andrea <andreamariaromero.daw@ciudadescolarfp.es>
2022-04-22 13:38:57 +0200

    Desde la rama Andrea creo un constructor con los tres parámetros

--- Ejercicios_GIT/src/Ejercicios_GIT/Alumno.java
+++ Ejercicios_GIT/src/Ejercicios_GIT/Alumno.java
@@ -4,4 +4,13 @@ public class Alumno {
 	private String nombre;
 	private String apellido;
 	private String dni;
+	
+	//Constructor
+	public Alumno(String nombre, String apellido, String dni) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+	}
+	
+	
 }
